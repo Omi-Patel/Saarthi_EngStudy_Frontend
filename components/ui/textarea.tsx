99f1c239b1e200a203143a4fd393c364a9844f74
Om Patel <omipatel7113@gmail.com>
2024-11-03 17:40:50 +0530

    fixes ts issues

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-// @ts-ignore
+// @ts-nocheck
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
