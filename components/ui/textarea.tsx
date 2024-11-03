0faae79e76d9492ae23880c1acd8c74dd2cd4e90
Om Patel <omipatel7113@gmail.com>
2024-11-03 17:37:34 +0530

    fixing issues by ignore ts

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
-// eslint-disable @typescript-eslint/no-empty-interface
+// @ts-ignore
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
