caa17142b30d0c712bd88815ee7ff51d604a885e
Om Patel <omipatel7113@gmail.com>
2024-11-03 17:30:38 +0530

    ignore ts- fixing issues

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-export interface InputProps
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -18,9 +18,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
